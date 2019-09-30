6bd620ca7d5478e1d4cca78fe8d49c9e53ca5368
CallumGrindle <callumgrindle@gmail.com>
2019-09-30 17:21:12 +0100

    end of day commit, everything functional

--- helpers/search.rb
+++ helpers/search.rb
@@ -26,17 +26,3 @@ def save_album(album_title, n)
   album.save
   return album.id
 end
-
-# def save_album
-#   album_search = RSpotify::Album.search(album_title)
-#   album_spotify_object = album_search[0]
-#   artist_details = {'name' => album_spotify_object.artists.first.name}
-#   if Artist.artist_exists?(artist_details['name']) == false
-#     artist = Artist.new(artist_details)
-#   elsif Artist.artist_exists?(artist_details['name']) == true
-#     artist = Artist.find(artist_details['name'])
-#   end
-#   album_details = {'title' => album_spotify_object.name, 'release_date' => album_spotify_object.release_date, 'artist_id' => artist.id, 'art' => album_spotify_object.images[0]['url']}
-#   album = Album.new(album_details)
-#   return found_album = {album_title: album.title, artist_name: artist.name, art: album.art}
-# end
